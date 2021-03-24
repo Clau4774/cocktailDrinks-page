502f41e5029735c4fc5d3cf92d6681d92015e223
clau4774 <claudiodangiolo@gmail.com>
2021-03-24 12:55:44 -0300

    Chargin the cocktail-grid with a json file to do this dinamicaly, I have to do the same with the aside

--- code/code.js
+++ code/code.js
@@ -1,20 +1,26 @@
 const hamMenu = document.querySelector('.ham-menu');
 const mobMenu = document.querySelector('.mobile-menu');
 
-
 function openCloseMenu() {
     mobMenu.classList.toggle('close-open-menu');
 }
 
 hamMenu.addEventListener('click', openCloseMenu);
 
-fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-    .then(res => {
-        if (res.ok) {
-        res.json();
-        console.log('res')
-        } else {
-            console.log('Not Succesful');
-        };
-    })
-    .then(data => console.log(data))
+createGrid();
+
+async function createGrid() {
+    const drinksGridGroup = document.querySelector('.cocktail-container__group');
+    const fetching = await fetch('code/drinks-json-grid.json');
+    const parsing = await fetching.json();
+    parsing.forEach((elem) => {
+        drinksGridGroup.innerHTML += `<div class="cocktail-container">
+        <a href="#">
+            <div class="cocktail-container__image">      
+                <img src="${elem.img}" alt="${elem.alt}">                        
+            </div>
+            <p>${elem.title}</p>
+        </a>
+    </div>`
+    });
+}
